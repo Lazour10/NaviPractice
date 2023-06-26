e8a9a92b55e3c481937e87c093cac41c04f02578
Camilo_Cydeo <Camilorojas8@knights.ucf.edu>
2023-06-26 18:21:22 -0400

    Created package and refactored Testing class

---
+++ src/naviGroup/camilo/Testing.java
@@ -0,0 +1,13 @@
+package naviGroup.camilo;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Testing {
+    public static void main(String[] args) {
+
+
+        String test = "Testing 1 2 3...";
+        System.out.println(test);
+
+    }
+}
